081ee3f45e28d444868681a98649b55a1cc7c49f
unknown <will@DESKTOP-6BTR423.lab.will.net>
2026-05-03 20:36:19 -0400

    Add valac (Vala compiler) to avizo build dependencies

--- install.sh
+++ install.sh
@@ -124,7 +124,7 @@ if ! command -v avizo-service &>/dev/null; then
     info "Building avizo from source..."
     # Install build dependencies from unstable to match upgraded libraries
     sudo DEBIAN_FRONTEND=noninteractive apt-get install -y -t unstable \
-        meson ninja-build libgtk-layer-shell-dev libgtk-3-dev libdbus-1-dev
+        meson ninja-build valac libgtk-layer-shell-dev libgtk-3-dev libdbus-1-dev
     tmpdir=$(mktemp -d)
     git clone https://github.com/misterdanb/avizo.git "$tmpdir/avizo"
     cd "$tmpdir/avizo"
